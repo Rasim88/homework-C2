5e849ea05c1d7ae8b808b86ec48eaffbe799727c
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2022-08-28 19:32:27 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// Задачи 10 и 13
+Console.WriteLine("Enter any number from 0 to 999:");
+int AnyNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (AnyNumber <= 9)
+    {
+        Console.WriteLine("This is a single digit number: " + AnyNumber);
+    }
+    else if (AnyNumber > 9 && AnyNumber < 100)
+    {
+        Console.WriteLine("Second digit: " + (AnyNumber % 10));
+
+        Console.WriteLine("Third digit is missing");
+    }
+    else if (AnyNumber >= 100)
+    {
+        Console.WriteLine("Second digit: " + ((AnyNumber / 10) % 10));
+
+        Console.WriteLine("Third digit:" + ((AnyNumber / 10) - ((AnyNumber / 10) % 1)));
+    }
+
+// Задачи 15
+
+Console.WriteLine("Enter Number from 0 to 7:");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+    if (Number == 1)
+    {
+        Console.WriteLine("It's a weekday - Monday: " + Number);
+    }
+    else if (Number == 2)
+    {
+        Console.WriteLine("It's a weekday - Tuesday: " + Number);
+    }
+    else if (Number == 3)
+    {
+        Console.WriteLine("It's a weekday - Wednesday: " + Number);
+    }    
+    else if (Number == 4)
+    {
+        Console.WriteLine("It's a weekday - Thursday: " + Number);
+    }    
+    else if (Number == 5)
+    {
+        Console.WriteLine("It's a weekday - Friday: " + Number);
+    }    
+    else if (Number == 6)
+    {
+        Console.WriteLine("It's a weekend - Saturday: " + Number);
+    }
+    else if (Number == 7)
+    {
+        Console.WriteLine("It's a weekend - Sunday: " + Number);
+    }
+
+// Дополнительная задача
+
+Console.WriteLine("Enter any number:");
+int MultiNumber = Convert.ToInt32(Console.ReadLine());
+
+    if ((MultiNumber % 7 == 0) && (MultiNumber % 23 == 0))
+    {
+        Console.WriteLine("You number is a multiple 7 and 23:" + MultiNumber);
+    }
+    else if (MultiNumber % 7 == 0)
+    {
+        Console.WriteLine("You number is a multiple of only 7: " + MultiNumber);
+    }
+    else if (MultiNumber % 23 == 0)
+    {
+        Console.WriteLine("You number is a multiple of only 23: " + MultiNumber);
+    }
+    else 
+    {
+        Console.WriteLine("You number is not multiple 7 and 23: " + MultiNumber);
+    }
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# homework-C2
+// Задача 1
+Console.WriteLine("Enter two numbers:");
+int NumberFirst = Convert.ToInt32(Console.ReadLine());
+int NumberSecond = Convert.ToInt32(Console.ReadLine());
+    if (NumberFirst > NumberSecond)
+    {
+        Console.WriteLine("Max Number: " + NumberFirst);
+    }
+    else if (NumberFirst < NumberSecond)
+    {
+        Console.WriteLine("Max Number: " + NumberSecond);
+    }
+    else
+    {
+        Console.WriteLine("The numbers are equal!");
+    }
+
+// Задача 2
+Console.WriteLine("Enter three numbers:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int Number3 = Convert.ToInt32(Console.ReadLine());
+    if (Number1 > Number2 && Number1 > Number3)
+    {
+        Console.WriteLine("Max Number: " + Number1);
+    }
+    else if (Number1 < Number2 && Number1 > Number3)
+    {
+        Console.WriteLine("Max Number: " + Number2);
+    }
+    else if (Number1 < Number2 && Number2 < Number3)
+    {
+        Console.WriteLine("Max Number: " + Number3);
+    }
+    else if (Number1 == Number2 && Number1 == Number3)
+    {
+        Console.WriteLine("The three numbers are equal!");
+    }
+
+// Задача 3
+Console.WriteLine("Enter one numbers:");
+int NumberOne = Convert.ToInt32(Console.ReadLine());
+    if (NumberOne % 2 == 0)
+    {
+        Console.WriteLine("An even Number: " + NumberOne);
+    }
+    else
+    {
+        Console.WriteLine("Odd Number: " + NumberOne);
+    }
+
+// Задача 4
+Console.WriteLine("Enter N number:");
+int NumberN = Convert.ToInt32( Console.ReadLine());
+for (int i = 1; i <= NumberN; i += 1)
+{
+if (i % 2 == 0)
+Console.WriteLine("An even numbers: " + i);
+}
